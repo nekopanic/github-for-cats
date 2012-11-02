f79bfedbf017f67a12f7baaecbb46908e5a65c06
edward <edward@edward-ThinkPad-X60s.(none)>
2012-11-02 16:33:15 +0100

    a pointless, but working bit of js to test pull requests

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+
+//pointless test of pull request from emccaughan,
+// these lines work and can be accepted
+if(1<100) {
+        console.log("reality is broken, please upgrade to ");
+}
